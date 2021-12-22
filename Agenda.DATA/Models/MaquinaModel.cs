b1d19025a454c392090e3705cb35a90c5e8fc27f
BOMBEIROSSP\32263773886 <victoramaro@policiamilitar.sp.gov.br>
2021-12-22 14:54:05 -0300

    Servico ManterMaquina.

---
+++ Agenda.DATA/Models/MaquinaModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda.DATA.Models
+{
+    public class MaquinaModel
+    {
+        public int maqCod { get; set; }
+        public int maqMarca { get; set; }
+        public int maqModelo { get; set; }
+        public int maqObse { get; set; }
+        public int maqStatus { get; set; }
+        public int diamCod { get; set; }
+        public int veicCod { get; set; }
+    }
+}
--- Agenda.DATA/Repositories/AgendaRepository.cs
+++ Agenda.DATA/Repositories/AgendaRepository.cs
@@ -2,11 +2,14 @@
 using JWT.Controllers;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 using System.Text;
+using System.Xml;
 
 namespace Agenda.DATA.Repositories
 {
@@ -25,6 +28,67 @@ namespace Agenda.DATA.Repositories
             _bdAgenda = Configuration.GetValue<string>("AgendaServicos");
         }
         #endregion
+
+        public string InsereLog(object valores, int usuCod, int tipLogCod, DateTime dataHoraTransacao)
+        {
+            string ret = "";
+            try
+            {
+                StringWriter sw = new StringWriter();
+                XmlTextWriter xw = new XmlTextWriter(sw);
+                XmlDocument xmlDocument = JsonConvert.DeserializeXmlNode(JsonConvert.SerializeObject(valores), "Valores");
+                xmlDocument.WriteTo(xw);
+                string objXML = sw.ToString() != "<Valores />" ? sw.ToString() : null;
+
+                using (SqlConnection connection = new SqlConnection(_ConnAgenda))
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    SqlTransaction transaction;
+                    transaction = connection.BeginTransaction("Transaction");
+                    command.Connection = connection;
+                    command.Transaction = transaction;
+
+                    try
+                    {
+                        //DateTime dataHoraTransacao = DateTime.Now;
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@logDesc", objXML);
+                        command.Parameters.AddWithValue("@logDataHora", dataHoraTransacao);
+                        command.Parameters.AddWithValue("@usuCod", usuCod);
+                        command.Parameters.AddWithValue("@tipLogCod", tipLogCod);
+
+                        command.CommandText = @"
+                                                    INSERT INTO " + _bdAgenda + @".dbo.LogSis
+                                                    (logDesc, logDataHora, usuCod, tipLogCod)
+                                                    VALUES(
+                                                    @logDesc,
+                                                    @logDataHora,
+                                                    @usuCod,
+                                                    @tipLogCod
+                                                    );
+                                                ";
+                        command.ExecuteNonQuery();
+
+                        //-> Finaliza a transação.
+                        transaction.Commit();
+                        ret = "OK";
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        connection.Close();
+                        ret = ex.Message;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ret = ex.Message;
+            }
+
+            return ret;
+        }
 
         public string Teste(string teste)
         {
@@ -66,21 +130,8 @@ namespace Agenda.DATA.Repositories
                         if (dt.Rows.Count > 0)
                         {
                             //-> Se existir, faz insere na tabela de Log.
-                            command.Parameters.Clear();
-                            command.Parameters.AddWithValue("@logDesc", "Login de " + objLogin.usuLogin);
-                            command.Parameters.AddWithValue("@logDataHora", dataHoraTransacao);
-                            command.Parameters.AddWithValue("@usuCod", dt.Rows[0].ItemArray[0]);
-                            command.Parameters.AddWithValue("@tipLogCod", 1); //-> Tipo de Log de Acesso.
-                            command.CommandText = @"
-                                                    INSERT INTO " + _bdAgenda + @".dbo.LogSis
-                                                    (logDesc, logDataHora, usuCod, tipLogCod)
-                                                    VALUES(
-                                                    @logDesc, 
-                                                    @logDataHora, 
-                                                    @usuCod, 
-                                                    @tipLogCod);
-                                                ";
-                            command.ExecuteNonQuery();
+                            object obj = JObject.Parse("{\n    \"Login\":\"" + objLogin.usuLogin + "\"\n}");
+                            InsereLog(obj, int.Parse(dt.Rows[0].ItemArray[0].ToString()), 1, dataHoraTransacao); //-> Tipo de Log de Acesso = 1.
                         }
                         else
                         {
@@ -119,6 +170,86 @@ namespace Agenda.DATA.Repositories
             }
 
             return objUsuarioRetorno;
+        }
+
+        public string ManterMaquina(MaquinaModel objMaquina)
+        {
+            string ret = "";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_ConnAgenda))
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    SqlTransaction transaction;
+                    transaction = connection.BeginTransaction("Transaction");
+                    command.Connection = connection;
+                    command.Transaction = transaction;
+
+                    try
+                    {
+                        DateTime dataHoraTransacao = DateTime.Now;
+                        command.Parameters.Clear();
+                        command.Parameters.AddWithValue("@maqCod", objMaquina.maqCod);
+                        command.Parameters.AddWithValue("@maqMarca", objMaquina.maqMarca);
+                        command.Parameters.AddWithValue("@maqModelo", objMaquina.maqModelo);
+                        command.Parameters.AddWithValue("@maqObse", objMaquina.maqObse);
+                        command.Parameters.AddWithValue("@maqStatus", objMaquina.maqStatus);
+                        command.Parameters.AddWithValue("@diamCod", objMaquina.diamCod);
+                        command.Parameters.AddWithValue("@veicCod", objMaquina.veicCod);
+
+                        //-> Se tiver id, faz Update:
+                        if (objMaquina.maqCod > 0)
+                        {
+                            command.CommandText = @"
+                                                        UPDATE " + _bdAgenda + @".dbo.Maquina
+                                                        SET 
+                                                        maqMarca=@maqMarca, 
+                                                        maqModelo=@maqModelo, 
+                                                        maqObse=@maqObse, 
+                                                        maqStatus=@maqStatus, 
+                                                        diamCod=@diamCod, 
+                                                        veicCod=@veicCod
+                                                        WHERE maqCod=@maqCod;
+                                                    ";
+                            command.ExecuteNonQuery();
+                        }
+                        //-> Senão, insere.
+                        else
+                        {
+                            command.CommandText = @"
+                                                        INSERT INTO " + _bdAgenda + @".dbo.Maquina
+                                                        (maqMarca, maqModelo, maqObse, maqStatus, diamCod, veicCod)
+                                                        VALUES(
+                                                        @maqMarca, 
+                                                        @maqModelo, 
+                                                        @maqObse, 
+                                                        @maqStatus, 
+                                                        @diamCod, 
+                                                        @veicCod
+                                                        );
+                                                    ";
+                            command.ExecuteNonQuery();
+                        }
+
+                        //-> Finaliza a transação.
+                        transaction.Commit();
+                        ret = "OK";
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        connection.Close();
+                        ret = ex.Message;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ret = ex.Message;
+            }
+
+            return ret;
         }
     }
 }
