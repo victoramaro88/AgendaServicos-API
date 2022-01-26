bc0ffa4884eefe69ef9b63640744097853afd583
BOMBEIROSSP\32263773886 <victoramaro@policiamilitar.sp.gov.br>
2022-01-26 08:33:19 -0300

    Início dos serviços de Evento.

---
+++ Agenda.DATA/Models/EventoModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agenda.DATA.Models
+{
+    public class EventoModel
+    {
+        public int eventCod { get; set; }
+        public string eventDesc { get; set; }
+        public string eventLogr { get; set; }
+        public string eventBairr { get; set; }
+        public DateTime eventDtIn { get; set; }
+        public DateTime evenDtFi { get; set; }
+        public string eventObse { get; set; }
+        public bool eventStatus { get; set; }
+        public int horaCod { get; set; }
+        public int cidaCod { get; set; }
+        public int diamCod { get; set; }
+        public int usuCod { get; set; }
+        public int maqCod { get; set; }
+    }
+}
